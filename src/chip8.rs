3b0c15620ef48d21f11ae7c6fed6658277c773ce
S Takumi <takumi.shiratori@gmail.com>
2023-03-30 18:12:08 +0900

    impl new

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,15 +1,35 @@
-const MEMORY_SIZE: isize = 4096;
-const V_SIZE: isize = 16;
-const GFX_SIZE_COL: isize = 64;
-const GFX_SIZE_ROW: isize = 32;
-const GFX_SIZE: isize = GFX_SIZE_COL * GFX_SIZE_ROW;
-const STACK_SIZE: isize = 16;
-const KEY_NUM: isize = 16;
+const MEMORY_SIZE: usize = 4096;
+const V_SIZE: usize = 16;
+const GFX_SIZE_COL: usize = 64;
+const GFX_SIZE_ROW: usize = 32;
+const GFX_SIZE: usize = GFX_SIZE_COL * GFX_SIZE_ROW;
+const STACK_SIZE: usize = 16;
+const KEY_NUM: usize = 16;
 
-struct Chip8 {
+const CHIP8_FONTSET: [u8; 80] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
+
+#[derive(Debug)]
+pub struct Chip8 {
     memory: [u8; MEMORY_SIZE],
-    V: [u8; V_SIZE],
-    I: u16,
+    v: [u8; V_SIZE],
+    i: u16,
     pc: u16,
     gfx: [u8; GFX_SIZE],
     delay_timer: u8,
@@ -17,4 +37,25 @@ struct Chip8 {
     stack: [u16; STACK_SIZE],
     sp: u16,
     key: [u8; KEY_NUM],
+}
+
+impl Chip8 {
+    pub fn new() -> Chip8 {
+        let mut initial_memory = [0; MEMORY_SIZE];
+        for i in 0..80 {
+            initial_memory[i] = CHIP8_FONTSET[i];
+        }
+        Chip8 {
+            memory: initial_memory,
+            v: [0; V_SIZE],
+            i: 0,      // Reset inex reister
+            pc: 0x200, // Program cunter starts at 0x200
+            gfx: [0; GFX_SIZE],
+            delay_timer: 0,
+            sound_timer: 0,
+            stack: [0; STACK_SIZE],
+            sp: 0, // Rese stack posinter
+            key: [0; KEY_NUM],
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
+use chip8::Chip8;
 use std::process::Command;
 
+mod chip8;
+
 fn main() {
-    println!("Hello, world!");
+    // Set up render system and resiger input callbacks
+    // setupGraphics
+    // setupInput
+
+    // Initialize the Chip8 system and load the game into the memory
+    let my_chip8 = Chip8::new();
+    // my_chip8.load_game("pong");
+
+    loop {
+        // Emulate one cycle
+        // my_chip8.emulate_cycle();
+
+        // If the draw flag is set, update the screen
+        // if my_chip8.drawFlag() {
+        //     my_chip8.drawGraphics();
+        // }
+
+        // Store kye press state (Press and Release)
+        // my_chip8.set_keys();
+    }
+
+    /*
     Command::new("clear").status().unwrap();
     for _i in 0..5 {
         for _j in 0..63 {
@@ -9,4 +33,5 @@ fn main() {
         }
         println!("");
     }
+    */
 }
